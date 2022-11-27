5cf2fe69bb89cdb381314a405c6380ec9652cf64
Breyson2811 <bpenar@unsa.edu.pe>
2022-11-27 15:32:22 -0500

    probando metodo foo con persona

--- Java/main.java
+++ Java/main.java
@@ -2,5 +2,12 @@ class Main{
   public static void main(String[] args){
     Persona p = new Persona("Pedro", 19);
     p.cantar();
+    Alumno a  = new Alumno("Jose", 14, 123);
+    a.show();
+
+    Y y = new Y();
+    y.foo(p);
+    System.out.println("aaaa");
+    y.foo(a);
   }
 } 
